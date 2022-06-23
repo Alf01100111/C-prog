becf0c6d8a7bb2ab6633005057f77ccded2d3cb5
Khukharev Andrey <107508594+Alf01100111@users.noreply.github.com>
2022-06-23 15:22:15 +0300

    Update Program.cs
    друзья и собака по схеме

--- exemple002sum/Program.cs
+++ exemple002sum/Program.cs
@@ -1,6 +1,31 @@
-﻿int numberA = new Random().Next(1,10); // 1 2 3 ... 9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10); // 1 2 3 ... 9;
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+//int numberA = new Random().Next(1,10); // 1 2 3 ... 9
+//Console.WriteLine(numberA);
+//int numberB = new Random().Next(1,10); // 1 2 3 ... 9;
+//Console.WriteLine(numberB);
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+int count = 0;
+int distance = 10000;
+int ffspeed = 1;
+int sfspeed = 2;
+int dogspeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+  if(friend == 1)
+  {
+    int time = distance/(ffspeed + dogspeed);
+    friend = 2;
+  }
+  if(friend == 2)
+  {
+    int time = distance/(sfspeed + dogspeed);
+    friend = 1;    
+  }
+  distance = distance - (ffriend + sfriend) * time;
+  count += 1;
+}
+Console.Write("Собака пробежит: ");
+Console.WriteLine(count);
