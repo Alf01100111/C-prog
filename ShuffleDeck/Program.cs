8de5e349dd847c9c3046643980a77fe56f7bdd07
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-29 23:16:12 +0300

    начало работы над shuffle deck

---
+++ ShuffleDeck/Program.cs
@@ -0,0 +1,15 @@
+﻿// void Shuffle(int[] arr)
+// {
+//     // создаем экземпляр класса Random для генерирования случайных чисел
+//     Random rand = new Random();
+
+//     for (int i = arr.Length - 1; i >= 1; i--)
+//     {
+//         int j = rand.Next(i + 1);
+
+//         int tmp = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = tmp;
+//     }
+// }
+
