c01cdc5027f97c2bcf34d55a27456a2798232273
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-02 11:49:24 +0300

    4 задача

---
+++ Seminar 3 2task/Program.cs
@@ -0,0 +1,23 @@
+﻿
+void writediap(int plane)
+{
+    if (plane == 1)
+    {
+        System.Console.WriteLine("x>0 y>0");
+    }
+    else if (plane == 2)
+    {
+        System.Console.WriteLine("x<0 y>0");
+    }
+    else if (plane == 3)
+    {
+        System.Console.WriteLine("x<0 y<0");
+    }
+    else if (plane == 4)
+    {
+        System.Console.WriteLine("x>0 y<0");
+    }
+    else System.Console.WriteLine("неверныый ввод");
+}
+
+writediap(2);
---
+++ Seminar 3 4task/Program.cs
@@ -0,0 +1,12 @@
+﻿void sqrtable(int n)
+{
+    int index = 1;
+    while (index <= n)
+    {
+        int result = index*index;
+        System.Console.Write($" {result}");
+        index++;
+    }
+}
+
+sqrtable(8);
