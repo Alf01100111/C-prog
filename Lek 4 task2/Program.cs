ff0215eea539df45d8c097809490056b0f28c9fd
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-02 23:01:58 +0300

    задача про замену символов в тексте

---
+++ Lek 4 task2/Program.cs
@@ -0,0 +1,22 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string text2 = Method4(5, ";lkj");
+System.Console.WriteLine(text2);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} X {j} = {i * j}");
+    }
+    System.Console.WriteLine();
+}
