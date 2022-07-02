8299e69c13692e8a7f272b14f78b554d5b33792d
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-02 10:35:06 +0300

    3 задача

---
+++ Seminar 3 1task/Program.cs
@@ -0,0 +1,47 @@
+﻿int getPlanesNumber(float x, float y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    else if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        return 4;
+    }
+    return -1;
+}
+
+
+void printPlanesNumber(float x, float y)
+{
+    if (x > 0 && y > 0)
+    {
+        System.Console.WriteLine("плоскость 1");
+    }
+    else if (x < 0 && y > 0)
+    {
+        System.Console.WriteLine("плоскость 2");
+    }
+    else if (x < 0 && y < 0)
+    {
+        System.Console.WriteLine("плоскость 3");
+    }
+    else if (x > 0 && y < 0)
+    {
+        System.Console.WriteLine("плоскость 4");
+    }
+    else System.Console.WriteLine("неверные данные");
+}
+
+int planesNumber = getPlanesNumber(-1, -2);
+System.Console.WriteLine(planesNumber);
+
+printPlanesNumber(-1, -2);
