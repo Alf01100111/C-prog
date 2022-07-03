5b70706e69929e690165cc4ab64e960e92c3f15f
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-03 10:27:28 +0300

    задача 1 начало

---
+++ Seminar 4 1task/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int GetSum(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("ВВедите число");
+int num = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(GetSum(num));
+
+// Сумма чисел от 1 до а
