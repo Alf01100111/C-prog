c584670f5744d072725843de32f15e5c7263853d
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-03 12:20:12 +0300

    рандомный массив с длинной заказанной пользователем - 3 функции

---
+++ Seminar 4 4task/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] RandomArray8()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+int[] GetArrayRandom(int size)
+{
+    int[] rar = new int[size];
+    for (int i = 0; i < rar.Length; i++)
+    {
+        rar[i] = new Random().Next(0, 2);
+    }
+    return rar;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var items in arr)
+    {
+        System.Console.Write(items + " ");
+    }
+}
+
+System.Console.WriteLine("введите длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] Massiv = GetArrayRandom(size);
+PrintArray(Massiv);
+
