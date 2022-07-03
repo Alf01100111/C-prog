3c20c91be5410388c0ef0af95c4051d7b278cd42
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-03 11:01:51 +0300

    задача 2

--- Seminar 4 2task/Program.cs
+++ Seminar 4 2task/Program.cs
@@ -1,1 +1,11 @@
 ﻿
+System.Console.WriteLine("ВВедите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for (; num != 0; num/=10)
+{
+    count++;
+}
+
+System.Console.WriteLine(count);
