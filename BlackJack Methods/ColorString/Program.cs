457f60606300d8d6bd688ca51d8f78375d76270c
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-08 22:37:37 +0300

    цветные строчки

---
+++ BlackJack Methods/ColorString/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Hello ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.Write("Hello ");
+
+Console.ResetColor();
+
+Console.Write("Hello ");
