9533ffb84f59d5ea8eaa5f1d507e02534929a1b4
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-16 13:27:31 +0300

    sem 7

---
+++ Seminar 7 task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int row = Int32.Parse(Console.ReadLine());
+int column = Int32.Parse(Console.ReadLine());
+
+double [,] matrix = new double[row,column];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,10);
+        System.Console.Write(matrix[i,j] + "\t"); // \t табуляция \n новая строка
+    }
+    System.Console.WriteLine();
+}
