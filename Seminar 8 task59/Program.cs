09d72c079177aa311f22f9a832f971c2f0429e48
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-17 12:15:07 +0300

    seminar

---
+++ Seminar 8 task59/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");       
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindMinElement(int[,] matrix, ref int indexI, ref int indexJ)
+{
+    int min = int.MaxValue;
+}
