726f0f091a4e7625054c0435f395d97197d7b85b
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-23 12:01:35 +0300

    seminar

---
+++ Seminar 9 task 69/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void Expon(int A, int B, int count = 0, int result = 1)
+{
+    if (count == B)
+    {
+        System.Console.Write(result);
+        return;
+    }
+    result *= A;
+    count++;
+    Expon(A, B, count, result);
+}
+
+Expon(5, 5);
